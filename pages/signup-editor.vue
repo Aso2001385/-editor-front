ea0417df64dc480ee86a47564c309a20d338e1c0
Aso2001152 <2001152@s.asojuku.ac.jp>
2022-10-17 09:39:58 +0900

    Webエディタの作成

---
+++ pages/signup-editor.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="markdown-editor">
+    <MenuHeader />
+    <no-ssr>
+      <mavon-editor
+        :toolbars="markdownOption"
+        language="ja" style="height:85%"
+      />
+    </no-ssr>
+  </div>
+</template>
+
+<script>
+import MenuHeader from '~/components/MenuHeader.vue';
+// import MenuHeader from '~/components/MenuHeader.vue';
+  export default {
+    props: {
+        model: {
+            //  type: Object | null,
+            default: null,
+        }
+    },
+    data() {
+        return {
+            markdownOption: {
+                bold: true,
+                italic: true,
+                header: true,
+                underline: true,
+                strikethrough: true,
+                mark: true,
+                quote: true,
+                ol: true,
+                ul: true,
+                link: true,
+                imagelink: true,
+                code: true,
+                table: true,
+                fullscreen: true,
+                htmlcode: true,
+            },
+        };
+    },
+    components: { MenuHeader }
+};
+</script>
+
+<style scoped>
+  .markdown-editor {
+    width: 100%;
+    height: 100%;
+  }
+</style>
