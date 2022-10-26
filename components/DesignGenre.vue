0a544be155adb92e929957de2cbdfcd7c71a1a45
2001157 <2001157@s.asojuku.ac.jp>
2022-10-26 12:44:19 +0900

    Webエディター設定画面の作成途中

--- components/DesignGenre.vue
+++ components/DesignGenre.vue
@@ -1,12 +1,34 @@
 <template>
-  <v-row>
-    <v-col cols="2"> </v-col>
-    <nuxt-link to="/BasicSetting"> 基本設定</nuxt-link>
-    <nuxt-link to="/DesignSetting"> デザイン設定</nuxt-link>
-  </v-row>
+  <v-container>
+    <!-- <v-row>
+      <v-col cols="4">
+        <v-row><v-btn>基本設定</v-btn></v-row>
+        <v-row><v-btn>全体設定</v-btn></v-row>
+        <v-row v-for="item in killData" :key="item">
+          <v-btn>{{ item }}</v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row> -->
+    <!-- <ProjectList /> -->
+    <!-- </v-row> -->
+  </v-container>
 </template>
-<script>
-// import { defineComponent } from '@vue/composition-api'
 
-export default {}
+<script>
+// import ProjectList from '~/components/ProjectList.vue'
+
+export default {
+  data() {
+    return {
+      // killData: ['page1', 'page2', 'page3', 'page4'],
+    }
+  },
+  // methods: {
+  //   clickEvent(event) {
+  //     this.$emit('ChangeClick', event)
+  //   },
+  // },
+  // components: { ProjectList },
+}
 </script>
