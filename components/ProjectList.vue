36819c7d5ea8fb065e5453178ede2dde64e595d3
kiyama ryota <ryo3403kiyama@icloud.com>
2022-11-04 10:35:24 +0900

    ページをフォルダ別にして管理 途中

--- components/ProjectList.vue
+++ components/ProjectList.vue
@@ -1,51 +1,53 @@
 <template>
   <v-container style="background-color: #dcdcdc">
     <v-row>
-      <NuxtLink :to="project_link" class="white--text" style="text-decoration: none">
-        <v-row style="float: left">
-          <v-card style="background-color: white; height: 20%">
-            <div class="pt-5 pb-5" style="width: 100%" :class="ProjectData.PreviewBackColor">
-              <v-col
-                cols="12"
-                class="black--text"
-                style="height: 9rem; width: 60rem; margin-left: 1%; padding-bottom: 10rem"
-              >
-                <div
-                  id="container"
-                  class="warp"
-                  v-html="ProjectData.PreviewText"
-                  style="height: 5rem; width: 80%"
-                ></div>
-              </v-col>
-            </div>
+      <div
+        :to="project_link"
+        class="white--text"
+        style="text-decoration: none"
+        @click="RoutePages(ProjectData.PreviewId)"
+      >
+        <v-row style="float: left; background-color: white">
+          <!-- <v-card style="background-color: white; height: 20%"> -->
+          <div class="pt-5 pb-5" style="width: 100%" :class="ProjectData.PreviewBackColor">
             <v-col
               cols="12"
               class="black--text"
-              style="border-top: 1px solid lightgrey; background: transparent; overflow: hidden; width: 100%"
+              style="height: 9rem; width: 60rem; margin-left: 1%; padding-bottom: 10rem"
             >
-              <p
-                id="project-name"
-                style="
-                  font-size: 17px;
-                  margin-left: 2.5%;
-                  color: #212121;
-                  overflow: hidden;
-                  text-overflow: ellipsis;
-                  white-space: nowrap;
-                  width: 12rem;
-                "
-              >
-                {{ ProjectData.PreviewName }}
-              </p>
+              <div id="container" class="warp" v-html="ProjectData.PreviewText" style="height: 5rem; width: 80%"></div>
             </v-col>
-          </v-card>
+          </div>
+          <v-col
+            cols="12"
+            class="black--text"
+            style="border-top: 1px solid lightgrey; background: transparent; overflow: hidden; width: 100%"
+          >
+            <p
+              id="project-name"
+              style="
+                font-size: 17px;
+                margin-left: 2.5%;
+                color: #212121;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+                width: 12rem;
+              "
+            >
+              {{ ProjectData.PreviewName }}
+            </p>
+          </v-col>
+          <!-- </v-card> -->
         </v-row>
-      </NuxtLink>
+      </div>
     </v-row>
   </v-container>
 </template>
 
 <script>
+// import previews from '~/assets/previews.json'
+
 export default {
   props: {
     ProjectData: Object,
@@ -55,20 +57,28 @@ export default {
       markdown: '# Marked in the browserRendered by **marked**.',
       project_link: 'create-editor',
       EDITORSETTING: 1,
-      // lists: [
-      //   { id: 1, name: 'project1', text: 'project1の内容表示' },
-      //   { id: 2, name: 'project2', text: 'project2の内容表示' },
-      //   { id: 3, name: 'project3', text: 'project3の内容表示' },
-      //   { id: 4, name: 'project4', text: 'project4の内容表示' },
-      //   { id: 5, name: 'project5', text: 'project5の内容表示' },
-      // ],
     }
   },
   mounted() {
+    const userProjectId = localStorage.getItem('projectCreateUpdate')
     if (this.ProjectData.ProjectLink === this.EDITORSETTING) {
       // Webエディターデザイン設定のページでは遷移をせずhtml表示の部分に反映させるようにする
-      this.project_link = 'editor-setting'
+      // this.project_link = 'editor-setting'
+      this.project_link = `/projects/${userProjectId}/pages/${userProjectId}/settings`
+      // this.$router.push({ path: `/projects/${userProjectId}/pages/${userProjectId}/settings` })
     }
+  },
+  methods: {
+    RoutePages(value) {
+      // 押された瞬間にローカルに押されたページ(createなのかupdateなのかの判定に)
+      localStorage.setItem('projectCreateUpdate', value)
+      console.log(this.ProjectData)
+      if (this.ProjectData.ProjectLink === this.EDITORSETTING) {
+        this.$router.push({ path: `${this.project_link}` })
+      } else {
+        this.$router.push({ path: `/projects/${value}` })
+      }
+    },
   },
 }
 </script>
---
+++ pages/projects/_id/pages/_number/settings.vue
@@ -0,0 +1,194 @@
+<template>
+  <!-- <div class="markdown-editor"> -->
+  <v-flex>
+    <MenuHeader />
+    <client-only>
+      <v-btn color="grey lighten-3" style="z-index: 2; position: absolute; margin-left: 88%; top: 90%">
+        <v-icon large :class="colorName + '--text'" style="font-size: 35px"> mdi-eyedropper-variant</v-icon>
+        <div :class="colorName + '--text'">{{ colorName }}</div>
+      </v-btn>
+    </client-only>
+    <v-row style="position: absolute; z-index: 2; top: 4.8rem; width: 100%">
+      <v-col
+        cols="2"
+        style="padding-left: 5%; overflow: hidden !important; height: 93vh; overflow-y: auto; background-color: white"
+      >
+        <v-row class="pt-10"
+          ><a class="text-h6 black--text" style="background-color: transparent; font-weight: bold" @click="Setting(0)">
+            基本設定
+          </a></v-row
+        >
+        <v-row class="pt-10"
+          ><a class="text-h6 black--text" style="font-weight: bold" @click="Setting(1)">全体設定</a></v-row
+        >
+        <v-row v-for="item in killData" :key="item" class="pt-10" @click="Setting(1)">
+          <a class="text-h6 black--text">{{ item }}</a>
+        </v-row>
+      </v-col>
+      <v-col cols="4" style="overflow: hidden !important; height: 93vh; overflow-y: auto; background-color: white">
+        <EditorSetting v-if="OpenFlg === 0" style="background-color: white" />
+        <v-card
+          v-for="(preview, index) in default_previews"
+          v-else
+          :key="index"
+          class="mt-8 mb-2"
+          cols="4"
+          style="float: left; width: 80%; margin-left: 10%"
+          @click="selectGenre(preview)"
+        >
+          <ProjectList
+            :ProjectData="
+              (ProjectData = {
+                PreviewId: preview.id,
+                PreviewName: preview.name,
+                PreviewText: preview.text,
+                PreviewBackColor: preview.backgroundColor,
+                ProjectLink: PROJECT_LINK,
+              })
+            "
+          />
+        </v-card>
+      </v-col>
+      <v-col
+        id="OpenHtml"
+        cols="6"
+        style="overflow: hidden !important; height: 93vh; overflow-y: auto"
+        :class="UserProjectColor[0]"
+        v-html="UserProjectData"
+      ></v-col>
+    </v-row>
+  </v-flex>
+</template>
+
+<script></script>
+
+<script>
+import DemoWebEditor from '~/assets/demo-web-editor.json'
+import previews from '~/assets/previews'
+import MenuHeader from '~/components/MenuHeader.vue'
+import ProjectList from '~/components/ProjectList.vue'
+import EditorSetting from '~/components/EditorSetting.vue'
+
+export default {
+  data() {
+    return {
+      selectionGenre: 0,
+      // projectData: {},
+      UserProjectData: '<h1>タイトル</h1><h2>サブタイトル</h2>',
+      UserProjectColor: [],
+      designPreview: '1',
+      HtmlColor: '',
+      primary: 'green',
+      Secondary: 'black',
+      killData1: '<h1>タイトル</h1><h2>サブタイトル</h2>',
+      setBackColor: '',
+
+      PROJECT_LINK: 1,
+      markdownOption: {
+        bold: true,
+        italic: true,
+        header: true,
+        underline: true,
+        strikethrough: true,
+        mark: true,
+        quote: true,
+        ol: true,
+        ul: true,
+        link: true,
+        imagelink: true,
+        code: true,
+        table: true,
+        fullscreen: true,
+        htmlcode: true,
+      },
+      iconColor: 'black--text',
+      colorNumber: 1,
+      colorName: 'black',
+      provisionalData: '',
+      killData: [
+        'page1',
+        'page2',
+        'page3',
+        'page4',
+        'page5',
+        'page6',
+        'page7',
+        'page8',
+        'page9',
+        'page10',
+        'page11',
+        'page12',
+      ],
+      default_previews: null,
+      OPENSETTING: 0,
+      OpenFlg: 999,
+      HtmlContent: {},
+    }
+  },
+  mounted() {
+    var test = document.getElementById('OpenHtml')
+    test.id = 'html-css'
+    this.UserProjectData = DemoWebEditor[0].text
+    if (this.colorName === '') {
+      this.colorName = 'black'
+    }
+    const data = previews
+    this.default_previews = previews
+    this.UserProjectColor.push(previews[0].backgroundColor)
+    this.UserProjectColor.push(previews[0].primaryColor)
+    this.UserProjectColor.push(previews[0].secondaryColor)
+  },
+
+  methods: {
+    // EditorData() {
+    //   localStorage.setItem('MarkdownData', this.markData)
+    // },
+    Setting(value) {
+      if (this.OPENSETTING === value) {
+        this.OpenFlg = value
+      } else {
+        this.OpenFlg = value
+      }
+    },
+    color(value) {
+      if (value === 1) {
+        this.colorNumber = value + 1
+        this.colorName = 'red'
+        localStorage.setItem('MarkdownColor', this.colorName)
+      } else if (value === 2) {
+        this.colorNumber = value + 1
+        this.colorName = 'blue'
+        localStorage.setItem('MarkdownColor', this.colorName)
+      } else if (value === 3) {
+        this.colorNumber = value + 1
+        this.colorName = 'green'
+        localStorage.setItem('MarkdownColor', this.colorName)
+      } else if (value === 4) {
+        this.colorNumber = 0
+        this.colorName = 'purple'
+        localStorage.setItem('MarkdownColor', this.colorName)
+      } else {
+        this.colorNumber = 1
+        this.colorName = 'black'
+        localStorage.setItem('MarkdownColor', this.colorName)
+      }
+    },
+    selectGenre(value) {
+      this.primary = value.primaryColor
+      this.Secondary = value.secondary.Color
+      this.HtmlColor = value.backgroundColor
+    },
+  },
+  components: {
+    MenuHeader,
+    ProjectList,
+    EditorSetting,
+  },
+}
+</script>
+
+<style scoped>
+::v-deep #html-css h1 {
+  color: blue;
+}
+</style>
