c0861743f87c67da82482269e4d740b484b7f46d
Aso2001196 <2001196@s.asojuku.ac.jp>
2022-11-25 09:58:46 +0900

    ハンバーガーメニュー

---
+++ components/Ui.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <h1 class="site-title"><a href="./">クリ★スタ</a></h1>
+    <Hamburger />
+    <Contents />
+  </header>
+</template>
+<script>
+import Hamburger from '~/components/parts/headers/menu/buttons/Hamburger.vue'
+import Contents from '~/components/parts/headers/menu/contents/Index.vue'
+export default {
+  components: {
+    Hamburger,
+    Contents,
+  },
+}
+</script>
+<style lang="sass" scoped>
+.header
+  align-items: center
+  background-color: #1b1310
+  border-bottom: 1px solid #fff
+  color: #fff
+  display: flex
+  height: 74px
+  justify-content: space-between
+  padding: 0 120px
+</style>
---
+++ components/parts/headers/menu/buttons/Hamburger.vue
@@ -0,0 +1,73 @@
+<template>
+  <button class="burger-btn">
+    <!-- ③ハンバーガーボタン -->
+    <span class="bar bar_top"></span>
+    <span class="bar bar_mid"></span>
+    <span class="bar bar_bottom"></span>
+  </button>
+</template>
+
+<script></script>
+
+<style lang="sass" scoped>
+.burger-btn
+  display: block
+  height: 39px
+  position: relative
+  width: 39px
+  z-index: 3
+
+  /* codepenの挙動のため
+  background-color: transparent
+  border: none
+
+.bar
+  background-color: #fff
+  display: block
+  height: 1px
+  left: 50%
+  position: absolute
+  transform: translateX(-50%)
+  width: 20px
+
+.bar_top
+  top: 10px
+
+.bar_mid
+  height: 1.5px
+  top: 50.5%
+  transform: translate(-50%, -50%)
+
+.bar_bottom
+  bottom: 10px
+
+.burger-btn
+  &.close
+    .bar_top
+      transform: translate(-50%, 10px) rotate(45deg)
+      transition: transform 0.3s
+
+    .bar_mid
+      opacity: 0
+      transform: translate(-900px, 0px) scale(100, 1)
+      transition: 0.5s
+
+    .bar_bottom
+      transform: translate(-50%, -8px) rotate(-45deg)
+      transition: transform 0.3s
+
+  &.restore
+    .bar_top
+      transform: translate(-50%, 0px) rotate(0deg)
+      transition: transform 0.3s
+
+    .bar_mid
+      opacity: 1
+      transition: opacity 0.5s
+
+    .bar_bottom
+      transform: translate(-50%, 0px) rotate(0deg)
+      transition: transform 0.3s
+
+/* もどす
+</style>
