38393c654be3c556938786b65e4fef24cf246337
kiyama ryota <ryo3403kiyama@icloud.com>
2022-11-29 10:59:27 +0900

    pluginsの書き換えやyarn、mavoneditorのインストールなど

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,12 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    {
+      src: '@/plugins/vue-mavon-editor',
+      srr: false,
+    },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [
